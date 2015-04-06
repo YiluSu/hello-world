164d831ddb6c118a49ce2c81e3b8ea272fa7b817
Yilu Su <yilusu530@gmail.com>
2015-04-06 15:55:34 -0400

    add !

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('New Hello World\n');
+	res.end('New Hello World!\n');
 }).listen(1337, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:1337/');
